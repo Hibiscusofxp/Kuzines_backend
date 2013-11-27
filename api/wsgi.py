50767c532f5e0e03c5ac64c601ec39ad68f07e08
Max Zhao <interarticle@gmail.com>
2013-11-27 02:21:58 -0500

    WSGI fix

--- api/wsgi.py
+++ api/wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 import sys
 
-sys.path.insert(0,os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
